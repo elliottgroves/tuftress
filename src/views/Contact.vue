ea34b0c1d051b67a87a8edecf928ede0d59902a7
Elliott Groves <sevorge@gmail.com>
2023-06-01 00:28:08 -0400

    Mobile scroll fix and contact page polish

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -5,12 +5,22 @@
     </template>
     <template #content>
       <p>I take commissions on my <a href="https://instagram.com/thetuftress" target="_blank">Instagram</a> or by email at <a href="mailto:thetuftress@gmail.com">thetuftress@gmail.com</a>!</p>
-      <a href="https://instagram.com/thetuftress" target="_blank" class="mr-2">
-        <Button>Instagram Message Me</Button>
-      </a>
-      <a href="mailto:thetuftress@gmail.com" target="_blank">
-        <Button>Email Me</Button>
-      </a>
+      <div class="links-container">
+        <a href="https://instagram.com/thetuftress" target="_blank">
+          <Button role="link">Instagram Message Me!</Button>
+        </a>
+        <a href="mailto:thetuftress@gmail.com" target="_blank">
+          <Button role="link">Email Me!</Button>
+        </a>
+      </div>
     </template>
   </Card>
 </template>
+
+<style scoped>
+.links-container {
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+}
+</style>
