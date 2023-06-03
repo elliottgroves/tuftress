dfa88c6b7ad35179f679c81599a476b5520255f3
Elliott Groves <sevorge@gmail.com>
2023-06-02 20:43:10 -0400

    Type fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -44,7 +44,7 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   if (to.meta.title) {
-    document.title = to.meta.title
+    document.title = String(to.meta.title)
   }
   next()
 })
